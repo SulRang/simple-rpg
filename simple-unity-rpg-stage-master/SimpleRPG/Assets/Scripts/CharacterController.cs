d1711af0fdd3e5b4e0a8a242193baee58526dafc
sulrang <angelace1007@gmail.com>
2019-09-26 19:42:29 +0900

    simple-rpg

---
+++ simple-unity-rpg-stage-master/SimpleRPG/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float speed;
+    bool isJumping;
+    float jump_force;
+    Animator animator;
+    public ChatBoxController chatbox;
+    void Start()
+    {
+        speed = 5.0f;
+        jump_force = 4.0f;
+        animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(transform.position.x) > 8)
+        {
+            chatbox.id = 1;
+            chatbox.text.text = "Want to EXIT?";
+            chatbox.Open();
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            isJumping = true;
+        }
+        Move();
+        if (Mathf.Abs(transform.position.x) > 100)
+            Debug.Log("want to exit");
+    }
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal > 0)
+        {
+            animator.SetInteger("Move", 1);
+            transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if (moveHorizontal < 0)
+        {
+            animator.SetInteger("Move", 1);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else
+            animator.SetInteger("Move", 0);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal * speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void Jump()
+    {
+        animator.SetInteger("Jump", 1);
+        Debug.Log("jump");
+        GetComponent<Rigidbody2D>().velocity += (Vector2.up * jump_force);
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isJumping = false;
+        animator.SetInteger("Jump", 0);
+    }
+
+}
---
+++ simple-unity-rpg-stage-master/SimpleRPG/Assets/Scripts/ChatBoxController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBoxController : MonoBehaviour
+{
+    public InputField InputText;
+    public Text text;
+    private Rect windowRect = new Rect((Screen.width) / 2, (Screen.height) / 2, 200, 300);
+    private bool show = false;
+    public int id = 0;
+    public void ScanText()
+    {
+        text.text = InputText.text;
+    }
+
+    void OnGUI()
+    {
+        if (show)
+            windowRect = GUI.Window(id, windowRect, DialogWindow, text.text);
+    }
+
+    // This is the actual window.
+    void DialogWindow(int windowID)
+    {
+        if (windowID == 0)
+        {
+            float y = 20;
+            if (GUI.Button(new Rect(0, y, windowRect.width , 30), "OK"))
+            {
+                show = false;
+            }
+        }
+        else if(windowID == 1)
+        {
+            float y = 20;
+            if (GUI.Button(new Rect(0, y, windowRect.width, 30), "Yes"))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+                show = false;
+            }
+            if (GUI.Button(new Rect(0, y + 40, windowRect.width, 30), "No"))
+            {
+                Application.LoadLevel(0);
+                show = false;
+            }
+        }
+    }
+
+    // To open the dialogue from outside of the script.
+    public void Open()
+    {
+        show = true;
+    }
+}
